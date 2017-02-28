ac48f00ef7f4ebc7f8aab6ddc8b2541de036b4d4
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2017-02-28 17:06:30 +0100

    Set more useful header for the Django admin

--- model_validation_api/admin.py
+++ model_validation_api/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import (ValidationTestDefinition, ValidationTestCode, ValidationTestResult,
                      ScientificModel, ScientificModelInstance)
 
+admin.site.site_header = "HBP Validation Service administration"
 
 @admin.register(ValidationTestDefinition)
 class ValidationTestDefinitionAdmin(admin.ModelAdmin):
